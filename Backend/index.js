e7c2f50b4a1ca5fa57f7437ff842dca219c69af7
Ubuntu <ubuntu@ip-172-31-28-1.us-east-2.compute.internal>
2024-08-30 17:57:35 +0000

    Added Dockerfile and docker-compose.yml for containerization

--- Backend/index.js
+++ Backend/index.js
@@ -8,7 +8,12 @@ const app = express();
 
 DbConnection();
 
-app.use(cors());
+app.use(
+  cors({
+    origin:http://18.191.117.210:5173,
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use("/api/v1", router);
 app.get("/", (req, res) => {
